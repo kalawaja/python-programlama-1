de2383c050dcaad65c06bc9a985e8013ec21bcb5
Ramazan İLTER <remzilter@gmail.com>
2022-09-26 19:28:40 +0300

    commit lesson 2.2

--- dersler/str03.py
+++ dersler/str03.py
@@ -4,3 +4,9 @@ metinsel = input("Bir ifade yazın: ")
 print(metinsel[::-1]) 
 
 print(metinsel[::-2]) # bir karakter atlayarak tersten yazdırır
+
+_degiskenAdi = 1_000_000
+print(_degiskenAdi)
+
+len(print("Girilen ifade", len(metinsel), "karakterdir"))
+# len(obj) -> bir ifadenin karakter sayısını gösterir
