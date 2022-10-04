8f5e4928351f53332d0d877902bc2f72f254e046
Ramazan İLTER <remzilter@gmail.com>
2022-10-04 22:55:11 +0300

    commit sozlukGuncelle.py

--- dersler/ders05/sozlukGuncelle.py
+++ dersler/ders05/sozlukGuncelle.py
@@ -1,36 +1,36 @@
 
-"""ogrenci = dict()
-
-ogrID = input("Öğrenci ID Girin: ")
-adiSoyadi = input("Öğrenci Adı Soyadı Girin: ")
-yas = input("Öğrenci Yaşı Girin: ")
-bolumu = input("Öğrenci Bölümü Girin: ")
-
-ogrenci.update({
-    "ID": ogrID, 
-    "Adi_Soyadi": adiSoyadi, 
-    "Yas": yas, 
-    "Bolumu": bolumu
-})
-
-print("\nGirilen Bilgiler Kaydedildi\n", ogrenci.values()) """
-
 ogrenci = dict()
 
 ogrID = input("Öğrenci ID Girin: ")
 adiSoyadi = input("Öğrenci Adı Soyadı Girin: ")
 yas = input("Öğrenci Yaşı Girin: ")
-bolumu = input("Öğrenci Bölümü Girin: ")
+bolum = input("Öğrenci Bölümü Girin: ")
+
+ogrenci.update({
+    "ID": ogrID, 
+    "Adı Soyadı": adiSoyadi, 
+    "Yaşı": yas, 
+    "Bölümü": bolum
+})
+
+print("\nGirilen Bilgiler Kaydedildi\n", ogrenci.values())
+
+"""ogrenci = dict()
+
+ogrID = input("Öğrenci ID Girin: ")
+adiSoyadi = input("Öğrenci Adı Soyadı Girin: ")
+yas = input("Öğrenci Yaşı Girin: ")
+bolum = input("Öğrenci Bölümü Girin: ")
 
 ogrenci.update({
     "ID": ogrID, 
     "Adi_Soyadi": adiSoyadi, 
     "Yas": yas, 
-    "Bolumu": bolumu
+    "Bolumu": bolum
 })
 
 silinen = ogrenci.pop("id") # sozluk içindeki yas değerini sileriz
 print("Ogrenci Silindi, Silinen ID\t: ", silinen)
 print(ogrenci)
-
-print(ogrenci.clear()) # sozluk içindeki tüm değerleri sileriz
+# print("\nGirilen Bilgiler Kaydedildi\n", ogrenci.values())
+print(ogrenci.clear()) # sozluk içindeki tüm değerleri sileriz"""
