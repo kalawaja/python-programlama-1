ebeeb9d06334798dae6a50e0530490d3b31faefa
Ramazan İLTER <remzilter@gmail.com>
2022-10-04 22:16:44 +0300

    commit completed two examples in genelOrnek.py

--- dersler/ders05/genelOrnek.py
+++ dersler/ders05/genelOrnek.py
@@ -1,4 +1,5 @@
 
+# Örnek 1
 import random, string
 
 harf = string.ascii_lowercase # ascii_lowercase harfleri alır (küçük harflerden oluşan bir değişkene atadık)
@@ -17,24 +18,26 @@ for idno in range(adet):
 
 print(liste)
 
+# Örnek 2
 """import random, string
 
-harf = string.ascii_lowercase # ascii_lowercase harfleri alır (küçük harflerden oluşan bir değişkene atadık)
+harf = string.ascii_lowercase 
 adet = int(input("Kac Öğrenci Girilecek: "))
 
 ogrenci = {} 
 
 for idno in range(adet):
-    sayi = random.randint(100, 999) # 3 basamaklı rastgele sayı üretir
-    ozel = random.choice(string.punctuation) # özel karakteri rastgele oluşturur
-    karakter = random.choice(harf) # harf dizisinden bir karakteri rastgele oluşturur
+    sayi = random.randint(100, 999)
+    ozel = random.choice(string.punctuation)
+    karakter = random.choice(harf)
 
     ogrenci["ogrID"] = (str(sayi)+ozel+karakter)
+
     # liste.append(ogrID) 
     ogrenci["adisoyadi"] = input("Öğrenci Adı Soyadı Girin: ")
     ogrenci["bolumu"] = input("Öğrenci Bölümü Girin: ")
-    ogrenci["yas"] = input(input("Öğrenci Yaşı Girin: "))
+    ogrenci["yas"] = int(input("Öğrenci Yaşı Girin: "))
+    for key in ogrenci.keys():
+        print(ogrenci[key])
+    
     print(ogrenci)"""
-
-
-
