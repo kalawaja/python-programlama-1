045cbb5b1788f7d4f4739a250b077be3e8bc2d61
Ramazan İLTER <remzilter@gmail.com>
2022-10-09 14:29:58 +0300

    commit wrap text in appArmstrong.py

--- dersler/appArmstrong.py
+++ dersler/appArmstrong.py
@@ -1,7 +1,10 @@
 
 # Armstrong Sayı Python Programı (0-1000 Aralığı)
 
-""""Tüm basamaklarındaki rakamların sayı değerlerinin küpleri toplamı, kendisine eşit olan sayılara Armstrong sayı denir." (Örneğin; 153 = 1³ + 5³ + 3³ = 1 + 125 + 27 = 153) Bu bilgilere göre aşağıdaki sayılardan hangisi bir Armstrong sayıdır?"""
+""""Tüm basamaklarındaki rakamların sayı değerlerinin küpleri toplamı,
+kendisine eşit olan sayılara Armstrong sayı denir." 
+(Örneğin; 153 = 1³ + 5³ + 3³ = 1 + 125 + 27 = 153) 
+Bu bilgilere göre aşağıdaki sayılardan hangisi bir Armstrong sayıdır?"""
 
 def armstrong(sayi):
     """Armstrong sayı kontrolü"""
