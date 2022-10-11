791860474649893fb19d60c5a4c17425e0fa89de
Ramazan İLTER <remzilter@gmail.com>
2022-10-11 19:25:45 +0300

    commit init-t.py

---
+++ dersler/ders10/init-t.py
@@ -0,0 +1,53 @@
+
+class Bilisim:
+    liste = []
+    def __init__(self, sinifAdi):
+        while True:
+            print(""" --- {} Sınıfa Hoşgeldiniz --- 
+            1- Ekle
+            2- Sil
+            3- Güncelle
+            4- Çıkış""".format(sinifAdi))
+            secim = int(input("Seçiminiz: "))
+            if secim == 1:
+                self.ekle()
+            elif secim == 2:
+                self.sil()
+            elif secim == 3:
+                self.guncelle()
+            else:
+                self.cikis()
+
+    def yazdir(self):
+        for eleman in self.liste:
+            print(eleman)
+
+    def guncelle(self):
+        pass
+
+    def ekle(self):
+        brans = input("Branş Giriniz: ")
+        ogretmen = input("Öğretmen Giriniz: ")
+        sinif = input("Sınıf Giriniz: ")
+        self.liste.append(brans)
+        self.liste.append(ogretmen)
+        self.liste.append(sinif)
+        print(self.liste)
+
+    """def sil(self):
+        silBrans = input("Silmek istediğiniz branşı giriniz: ")
+        if silBrans in self.liste:
+                self.liste.pop(self.liste.index(silBrans))
+        print(self.liste)"""
+
+    def sil(self):
+        silBrans = input("Silmek istediğiniz branşı giriniz: ")
+        if silBrans in self.liste:
+            self.liste.remove(silBrans)
+        print(self.liste)
+
+sinifAdi = input("Sınıf Adı Giriniz: ")
+bolum1 = Bilisim(sinifAdi)
+
+
+# init ile iç içe 2 tane liste oluşturarak öğrenci bilgilerini yazdırma (ödev)):
