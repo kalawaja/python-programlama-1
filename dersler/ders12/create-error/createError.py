038f5742cb6e8f8bcc1b264833dd32cce37e38b7
Ramazan İLTER <remzilter@gmail.com>
2022-10-17 22:50:23 +0300

    commit edit createError.py

--- dersler/ders12/create-error/createError.py
+++ dersler/ders12/create-error/createError.py
@@ -1,13 +1,13 @@
 
 try:
-    s1 = input("Birinci Sayı: ")
-    s2 = input("İkinci Syaı: ")
+    s1 = int(input("Birinci sayi.: "))
+    s2 = int(input("İkinci  sayi.: "))
     bolum = s1 / s2
 except Exception as hata:
-    print("Hata: ", hata)
-
-    with open("python-programlama-1/dersler/ders12/create-error/hata.txt", "a", encoding="utf-8") as hatakaydi:
+    print("Hata →", hata)
+    
+    with open("hatali.txt", "a", encoding="utf-8") as hatakaydi:
         print(hata, file=hatakaydi)
 else:
-    with open("python-programlama-1/dersler/ders12/create-error/sonuc.txt", "a", encoding="utf-8") as sonuc:
+    with open("sonuc.txt", "a", encoding="utf-8") as sonuc:
         print("{}:{}={}".format(s1, s2, bolum), file=sonuc)
