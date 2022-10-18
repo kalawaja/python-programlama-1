b35e20642575737a1eebb21e2a495cf6a6e72ad6
Ramazan İLTER <remzilter@gmail.com>
2022-10-18 19:39:42 +0300

    commit update 100% student-note

--- dersler/ders13/student-note/main.py
+++ dersler/ders13/student-note/main.py
@@ -1,41 +1,41 @@
 
-# Kullanıcı adı ve parola bilgisi ilk başta dosyaya kaydolacak.
-# Kullanıcı adı ve parola girildiğinde kullanıcı ve parola doğruysa hoşgeldiniz mesajı ve;
-# Hatalı kullanıcı mesajı veren python programı:
+# Kullanıcı adı ve parola bilgisi ilk başta dosyaya kaydolucak.
+# Kullanıcı adı ve parola girildiğinde kullanıcı ve parola doğruysa hoşgeldiniz mesajı ve 
+# hatalı Kullanıcı mesajı veren python programı
 
 # from notlar import *
-import notlar
+import notlar 
 
 def kontrol(kullanici):
-    import os   # ekran temizlemek için os modülü import edildi.
-    bilgi = kullanici.split(" : ")
+    import os # ekran temizleme için os modülü dahil edilir  
+    bilgi = kullanici.split(":")
     # print(bilgi)
     while True:
-        # Sisteme giriş için bilgileri istiyoruz. (Sign in)
-        os.system("clear")    # ekran temizlendi.
-        print("Kullanıcı Girişi".center(30, "#"))
-        user = input("Kullanıcı adı: ")
-        password = input("Parola: ")
-
-        if user == bilgi[0] and password == bilgi[1]:
-            os.system("clear")    # ekran temizlendi.
-            print(f"Hoşgeldin {user}, Sisteme Giriş İzni Verildi. \n")
-            notlar.menu()
-            break
-        else:
-            print("Hatalı Giriş Yaptınız!")
-            break
+        # sisteme giriş için bilgileri istiyoruz LOG-IN
+        os.system("clear")
+        print(" Kullanıcı Girişi ".center(30,"#"))
+        user = input("Kullanici Adi: ")
+        password = input("Parola...: ")
         
+        if (user == bilgi[0]):
+            if (password == bilgi[1]):
+                os.system("clear")
+                print(f"Hoş Geldin {user}, Sisteme Giriş İzni Verildi \n")
+                notlar.menu()
+                break
+            else:
+                print("Hatalı Giriş")
+                continue   
+            
 def main():
     userList = []
-    # sisteme kullanıcı kaydı yapılıyor. (Sign up)
+    # sisteme yeni kullanıcı kaydı yapılıyor SIGN-IN
     with open("users.txt", "w", encoding="utf-8") as dosya:
-        user = input("Kullanıcı adı: ")
-        password = input("Parola: ")
-        userList.append(dosya.write(user + " : " + password))
-
+        user = input("Kullanici Adi Girin: ")
+        password = input("Parola Girin: ")
+        userList.append(dosya.write(user+":"+password))
+        
     with open("users.txt", encoding="utf-8") as dosya:
         for satir in dosya:
-            kontrol(satir)
-
+            kontrol(satir)           
 main()
--- dersler/ders13/student-note/notlar.py
+++ dersler/ders13/student-note/notlar.py
@@ -1,36 +1,88 @@
 
+
+def harfNotuHesapla(ogr):  # ali:50-70-90
+    ogrenci = ogr.split(":")
+    ogrAdi = ogrenci[0]  # ogrencinin adsoyad bilgilerini içeriyor
+    notlar = ogrenci[1].split("-")  # ogrencinin not bilgilerini içeriyor "-" den ayırdık
+    
+    vize = int(notlar[0])
+    final = int(notlar[1])
+    odev = int(notlar[2])
+    
+    ortalama = (odev * 0.2) + (vize * 0.3) + (final * 0.5)
+    
+    if(ortalama >= 85 and ortalama <=100):
+        harfNotu = "AA"
+    elif(ortalama >= 70 and ortalama <85):
+        harfNotu = "BB"
+    elif(ortalama >= 60 and ortalama <70):
+        harfNotu = "CC"
+    elif(ortalama >= 50 and ortalama <60):
+        harfNotu = "DD"
+    elif(ortalama >= 0 and ortalama <50):
+        harfNotu = "FF"
+    else:
+        print("Gecersiz Not!")
+    
+    # print("ortalama:",ortalama)  # ortalamasını terminalde gosterdik
+    return (ogrAdi + ": " + harfNotu)  # ogr parametresine karşılık buradaki değeri gönderdik
+
+def harfNotuKaydet():
+    try:
+        # listenin amacı: harfnotu fonks gelen değerleri listede sakla ve yeni bir dosyaya yaz
+        liste = []
+        with open("ogrnotlari.txt", encoding="utf-8") as notlar:
+            for ogrenci in notlar:
+                liste.append(harfNotuHesapla(ogrenci))
+            with open("harfNotlari.txt", "w", encoding="utf-8") as harfnotu:
+                for i in liste:
+                    harfnotu.write(i)
+                    harfnotu.write("\n")
+        print("Öğrenci Harf Notları Kaydedilmiştir.")
+                
+    except Exception as hata:
+        print("Hata Oluştu", hata)
+
+def notlariListele():
+    #dosya oluşturulmamışsa hata oluştur
+    try:
+        with open("ogrnotlari.txt", encoding="utf-8") as notlar:
+            for ogrenci in notlar:
+                print(harfNotuHesapla(ogrenci))              
+    except Exception as hata:
+        print("Hata Oluştu", hata)
+
+
 def notGirisi():
-    adSoyad = input("Öğrencinin adını ve soyadını giriniz: ")
-    vize = int(input("Vize notunu giriniz: "))
-    final = int(input("Final notunu giriniz: "))
-    odev = int(input("Ödev notunu giriniz: "))
+    adSoyad = input("Ad ve Soyad Girin.: ")
+    vize = int(input("Vize Notunu Girin.:"))
+    final = int(input("Final Notunu Girin.:"))
+    odev = int(input("Ödev Notunu Girin.:"))
 
     with open("ogrnotlari.txt", "a", encoding="utf-8") as notlar:
         notlar.write(adSoyad + ":" + str(vize) + "-" + str(final) + "-" + str(odev) + "\n")
 
-
 def menu():
     while True:
-        print(""" Öğrenci Not Otomasyon Sİstemi
-        1. Not Girişi 
+        print(""" ÖĞRENCİ NOT OTOMASYON SİSTEMİ
+        1. Not Girişi
         2. Harf Notu Listele
-        3. Notları Kaydet
-        4. Çıkış Yap """)
-
+        3. Harf Notu Kaydet 
+        4. Çıkış""")
+        
         try:
-            secim = int(input("Yapmak istediğiniz işlemi seçiniz: "))
-            if secim == 1:
+            secim = int(input("Seciminiz.: "))
+            if (secim == 1):
                 notGirisi()
-            elif secim == 2:
-                pass
-            elif secim == 3:
-                pass
-            elif secim == 4:
+            elif (secim == 2):
+                notlariListele()
+            elif (secim == 3):
+                harfNotuKaydet()
+            elif (secim == 4):
                 exit(0)
             else:
-                print("Hatalı Değer Girildi!")
+                print("Hatalı Değer Girildi")
 
         except Exception as hata:
-            print("Hatalı Değer Girildi!")
+            print("Hatalı Deger Girildi")
             continue
-        
