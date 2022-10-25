af3a52504ae52666f244243b8bee6a23775373d7
Ramazan İLTER <remzilter@gmail.com>
2022-10-26 00:52:21 +0300

    edit changes de morgan rules in appOnerme.py

--- dersler/appOnerme.py
+++ dersler/appOnerme.py
@@ -1,13 +1,43 @@
-# Matematikteki Mantık Konusundaki Bileşik Önermelerde p ve q ile örnekler
+# Matematik Mantık Konusundaki Bileşik Önermeler ile ilgili Python Programı
 
-"""# p için doğruluk değeri giriniz
+# "VE" Bağlacı ile Yapılan Bileşik Önermeler
+# "VEYA" Bağlacı ile Yapılan Bileşik Önermeler
+# "YA DA" Bağlacı ile Oluşturulan Bileşik Önermeler
+# "İSE" Bağlacı ile Oluşturulan Bileşik Önermeler
+# "ANCAK VE ANCAK" Bağlacı ile Kurulan Bileşik Önermeler
+
+# p için doğruluk değeri giriniz
 inputP = int(input("p için doğruluk değeri giriniz: "))
 
 # q için doğruluk değeri giriniz
 inputQ = int(input("q için doğruluk değeri giriniz: "))
 
-# p v q == için doğruluk değerlerini yazdır
-print("p v q == ", inputP + inputQ - (inputP * inputQ))
+# p v q ≡ için doğruluk değerini yazdır
+print("p ∨ q ≡ ", inputP + inputQ - (inputP * inputQ))
 
-# p ^ q == için doğruluk değerlerini yazdır
-print("p ^ q == ", inputP * inputQ)"""
+# p ^ q ≡ için doğruluk değerini yazdır
+print("p ^ q ≡ ", inputP * inputQ)
+
+# p ⊻ q ≡ için doğruluk değerini yazdır
+print("p ⊻ q ≡ ", inputP + inputQ - 2 * (inputP * inputQ))
+
+# p → q ≡ için doğruluk değerini yazdır
+print("p → q ≡ ", 1 - inputP + inputQ)
+
+# p ↔ q ≡ için doğruluk değerini yazdır
+print("p ↔ q ≡ ", 1 - abs(inputP - inputQ))
+
+# p ⇒ q önermesinin karşıtı q ⇒ p
+print("p ⇒ q önermesinin karşıtı q ⇒ p ≡ ", 1 - inputP + inputQ)
+
+# p ⇒ q önermesinin tersi p’ ⇒ q’
+print("p ⇒ q önermesinin tersi p’ ⇒ q’ ≡ ", inputP - inputQ)
+
+# p ⇒ q önermesinin karşıt tersi q’ ⇒ p’
+print("p ⇒ q önermesinin karşıt tersi q’ ⇒ p’ ≡ ", inputP - inputQ)
+
+# De Morgan (p ^ q)' ≡ p' ∨ q' için doğruluk değerini yazdır
+print("(p ^ q)' ≡ p' ∨ q' ≡ ", 1 - inputP + 1 - inputQ)
+
+# De Morgan (p ∨ q)' ≡ p' ^ q' için doğruluk değerini yazdır
+print("(p ∨ q)' ≡ p' ^ q' ≡ ", 1 - inputP * 1 - inputQ)
