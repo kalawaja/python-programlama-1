7e41a18fb6dcd5a46a390c808f9703e8b6605b92
Ramazan İLTER <remzilter@gmail.com>
2022-10-25 21:20:49 +0300

    add appOnerme.py

---
+++ dersler/appOnerme.py
@@ -0,0 +1,13 @@
+# Matematikteki Mantık Konusundaki Bileşik Önermelerde p ve q ile örnekler
+
+"""# p için doğruluk değeri giriniz
+inputP = int(input("p için doğruluk değeri giriniz: "))
+
+# q için doğruluk değeri giriniz
+inputQ = int(input("q için doğruluk değeri giriniz: "))
+
+# p v q == için doğruluk değerlerini yazdır
+print("p v q == ", inputP + inputQ - (inputP * inputQ))
+
+# p ^ q == için doğruluk değerlerini yazdır
+print("p ^ q == ", inputP * inputQ)"""
